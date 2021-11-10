e6b32be83f18edaf2b4a2dd69d49b75221b9246e
Nazim-Habib <c161067@ugrad.iiuc.ac.bd>
2021-11-10 19:16:36 +0600

    added Code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9s2cu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
@@ -18,6 +18,9 @@ async function run() {
         await client.connect();
         const database = client.db("Mahima-Motors");
         const userCollection = database.collection("users");
+        const productCollection = database.collection("products");
+        const orderCollection = database.collection("orders");
+        const reviewCollection = database.collection("reviews");
 
 
         // save user to database/ this function is used for register form
@@ -27,15 +30,53 @@ async function run() {
             console.log(result);
             res.json(result);
         });
+        // save products to database
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            console.log(result);
+            res.json(result);
+        });
 
-         // update user to database/ this function is used for google signing
-         app.put('/users', async (req, res) => {
+         // post reviews to the database
+         app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log(review)
+            const result = await reviewCollection.insertOne(review)
+            res.json(result)
+
+        });
+
+        // update user to database/ this function is used for google signing
+        app.put('/users', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
             const options = { upsert: true };
             const updateDoc = { $set: user };
             const result = await userCollection.updateOne(filter, updateDoc, options);
             res.json(result);
+        });
+
+        // get all products
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const product = await cursor.toArray();
+            res.send(product);
+        });
+
+        // get a single product frpm product collection
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.json(product);
+        });
+        // Save order into database
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result)
         });
 
     }
